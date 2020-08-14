f8c3c9fd4557a4baa88bea858085e65f022de9ce
kadismile <brianking319@gmail.com>
2020-08-14 18:47:16 +0100

    fixed errors on travis

--- test/auth-test.js
+++ test/auth-test.js
@@ -28,7 +28,7 @@ exports.authTest = () =>
             .send(authDetails)
             .then( async (res) => {
               chai.expect(res.status).to.eql(200); // expression which will be true if response status equal to 201
-              chai.assert.exists(res.body.data.token); // assertion expression which will be true if id exists
+              chai.assert.exists(res.body.token); // assertion expression which will be true if id exists
               done();
             })
             .catch(done);
