6ca7fd8928977f52dbcef369243081ddce48a553
kadismile <brianking319@gmail.com>
2020-08-18 00:25:01 +0100

    added permissions and protected routes

--- main_application/routes/auth.js
+++ main_application/routes/auth.js
@@ -1,10 +1,16 @@
 var router = require('express').Router();
 const authController = require('../controllers/authController');
+const { protect, authorize } = require('../middleware/auth');
+const permission =  require("../config/permissions");
 
 router.post('/church/register', authController.registerChurch);
-router.post('/user/register', authController.registerUser);
-router.post('/user/update', authController.updateUser);
+
+router.post('/user/register', protect, authorize(permission.USER_PERMISSION), authController.registerUser);
+
+router.post('/user/update',protect, authorize(permission.USER_PERMISSION), authController.updateUser);
+
 router.post('/forgetpassword', authController.forgotPassword);
+
 router.put('/resetpassword/:resettoken', authController.resetPassword);
 
 router.post('/login', authController.authLogin);
--- main_application/utils/errors.js
+++ main_application/utils/errors.js
@@ -2,7 +2,9 @@
 const ErrorResponse = require('../utils/errorResponse');
 
 module.exports.errorHandler = (err, res) => {
+    
     let error = {...err};
+    console.log("ERROR ___ ", error)
     error.message = err.message;
     if (err.type === 'entity.parse.failed') {
         const message = `Wrong Entity as been Parsed`;
